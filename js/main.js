c88a6635829f8b4e175cdd3060ce4c4e7510e5be
shibawanko0202 <97938730+shibawanko0202@users.noreply.github.com>
2022-04-19 16:31:00 +0900

    レイアウトの変更

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ const to_top = document.getElementById("to_top");
 
 // 位置を確認してクラスを制御
 function top_button(){
-  if(header.getBoundingClientRect().y < 0){
+  if(header.getBoundingClientRect().y < -100){
     to_top.classList.add("show");
   } else {
     to_top.classList.remove("show");
